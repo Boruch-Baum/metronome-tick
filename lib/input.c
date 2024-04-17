836be62f4da1fc95eacf1d75ad9b6034a11eccb2
Coco Liliace <coco@liliace.dev>
2024-04-17 11:38:57 +0800

    feat: support modifying input in prompt

--- lib/input.c
+++ lib/input.c
@@ -53,9 +53,73 @@ int get_input(void) {
 void get_command(char *line, char *prompt) {
 	printf("\n%s", prompt);
 	fflush(stdout);
-	reset_terminal_mode();
-	fgets(line, sizeof(line), stdin);
-	printf("\033[A\33[2K\033[A\33[2K"); // https://stackoverflow.com/a/35190285/10254049
-	prepare_for_input();
+	int c;
+	int i = 0;
+	int len = 0;
+	while (1) {
+		c = get_input();
+		// https://stackoverflow.com/a/26423857/10254049
+		if (c == ENTER_KEY) {
+			break;
+		} else if (c == RIGHT_ARROW_SUBSTITUTION) {
+			if (i < len) {
+				i += 1;
+				printf("\033[1C");
+				fflush(stdout);
+			}
+		} else if (c == LEFT_ARROW_SUBSTITUTION) {
+			if (i > 0) {
+				i -= 1;
+				printf("\033[1D");
+				fflush(stdout);
+			}
+		} else if (c == BACKSPACE_KEY) {
+			if (i > 0) {
+				for (int j = i - 1; j < len - 1; j++) {
+					line[j] = line[j+1];
+				}
+				i -= 1;
+				len -= 1;
+				line[len] = '\0';
+				printf("\033[1D");
+				printf("\033[K");
+				printf("%s", line+i);
+				if (len != i) { // "\033[0D" moves the cursor 1 to the right instead of 0
+					printf("\033[%dD", len-i);
+				}
+				fflush(stdout);
+			}
+		} else if (c == DELETE_KEY_SUBSTITUTION) {
+			if (i < len) {
+				for (int j = i; j < len - 1; j++) {
+					line[j] = line[j+1];
+				}
+				len -= 1;
+				line[len] = '\0';
+				printf("\033[K");
+				printf("%s", line+i);
+				if (len != i) {
+					printf("\033[%dD", len-i);
+				}
+				fflush(stdout);
+			}
+		} else if (len < MAX_COMMAND_LEN - 1) {
+			for (int j = len; j > i; j--) {
+				line[j] = line[j-1];
+			}
+			line[i] = c;
+			i += 1;
+			len += 1;
+			line[len] = '\0';
+			line[MAX_COMMAND_LEN-1] = '\0';
+			printf("\033[K");
+			printf("%s", line+i-1);
+			if (len != i) {
+				printf("\033[%dD", len-i);
+			}
+			fflush(stdout);
+		}
+	}
+	printf("\33[2K\033[A\33[2K");
 	// TODO handle SIGINT
 }
