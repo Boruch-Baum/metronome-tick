056beb2c12856043a319af321e8158d7a2a076f3
Coco Liliace <coco@liliace.dev>
2026-06-28 15:27:26 +0800

    Improve beep sound to more realistic ticks

--- lib/player.c
+++ lib/player.c
@@ -4,7 +4,15 @@
 #define SAMPLE_RATE 8000 // number of values per second
 #define PEAK_TO_PEAK 256 // 2^8 for uint8_t
 #define AMPLITUDE (PEAK_TO_PEAK / 2)
-#define SINE_FACTOR (2 * M_PI / (PEAK_TO_PEAK - 1))
+#define TICK_MS 22
+#define TICK_GAIN 2.0
+#define BODY_DECAY 125.0
+#define IMPULSE_FRAMES 4
+#define IMPULSE_LEVEL 120.0
+#define IMPULSE_STEP 28.0
+#define TONE_LEVEL 88.0
+#define HARMONIC_LEVEL 36.0
+#define HARMONIC_RATIO 2.7
 
 void prepare_player(snd_pcm_t **pcm) {
 	int rc = snd_pcm_open(pcm, "default", SND_PCM_STREAM_PLAYBACK, 0);
@@ -21,13 +29,29 @@ void prepare_player(snd_pcm_t **pcm) {
 		40000);      // latency
 }
 
+static uint8_t to_sample(double centered) {
+	double sample = AMPLITUDE + TICK_GAIN * centered;
+
+	if (sample < 0) {
+		return 0;
+	}
+	if (sample > PEAK_TO_PEAK - 1) {
+		return PEAK_TO_PEAK - 1;
+	}
+	return sample + 0.5;
+}
+
 uint8_t *create_waves(int *buff_size, struct PlayerState *ps) {
 	int tick_frame_size = SAMPLE_RATE * 60 / ps->bpm; // SAMPLE_RATE / (bpm / 60)
 	*buff_size = tick_frame_size * strlen(ps->rhythm);
 	uint8_t *buffer = malloc(*buff_size);
 	memset(buffer, AMPLITUDE, *buff_size);
+	int tick_frames = SAMPLE_RATE * TICK_MS / 1000;
+	if (tick_frames > tick_frame_size) {
+		tick_frames = tick_frame_size;
+	}
+	double body_step = exp(-BODY_DECAY / SAMPLE_RATE);
 	int freq;
-//      freq = 2500.0;     // click-like, higher than a normal beep
 	for (int i = 0; ps->rhythm[i] != '\0'; i++) {
 		switch (ps->rhythm[i]) {
 		case '>':
@@ -39,15 +63,18 @@ uint8_t *create_waves(int *buff_size, struct PlayerState *ps) {
 		default:
 			continue;
 		}
-                int offset = i * tick_frame_size;
-                double decay = 35.0;           // larger = shorter click
-                double phase_inc = 2.0 * M_PI * freq / SAMPLE_RATE;
-                for (int j = 0; j < tick_frame_size / 4; j++) {
-                    double t = (double)j / SAMPLE_RATE;
-                    double env = exp(-decay * t);
-                    double s = env * sin(phase_inc * j);
-                    buffer[offset + j] = (int)(AMPLITUDE * s) + AMPLITUDE;
-                }
+		int offset = i * tick_frame_size;
+		double phase_step = 2 * M_PI * freq / SAMPLE_RATE;
+		double phase = 0.0;
+		double body = 1.0;
+		for (int j = 0; j < tick_frames; j++) {
+			double impulse = j < IMPULSE_FRAMES ? IMPULSE_LEVEL - j * IMPULSE_STEP : 0;
+			double tone = TONE_LEVEL * sin(phase) + HARMONIC_LEVEL * sin(phase * HARMONIC_RATIO);
+
+			buffer[offset+j] = to_sample(impulse + tone * body);
+			body *= body_step;
+			phase += phase_step;
+		}
 	}
 	return buffer;
 }
